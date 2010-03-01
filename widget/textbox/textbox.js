7475de4e4f2165b60ab789f6cd533d56c3d1c627
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-03-01 19:38:59 +0000

    Added default classnames to the widgets.

--- widget/textbox/textbox.js
+++ widget/textbox/textbox.js
@@ -4,6 +4,7 @@ steal.plugins('phui/widget').then(function($){
     Phui.Widget.extend("Phui.Widget.Textbox",
     {
         defaults : {
+            CLASS_NAMES : "entry",
             INPUT_CLASS_NAMES : "",
             LABEL_CLASS_NAMES : "label",    
             DESCRIPTION_CLASS_NAMES : "description"
@@ -13,6 +14,7 @@ steal.plugins('phui/widget').then(function($){
     {
         init : function(el, options){
             this.options = options;
+            this.element.addClass(this.Class.CLASS_NAMES);
         },
         keypress : function(el, ev){
             if(this.options.allow && ev.charCode && ! this.options.allow.test(String.fromCharCode(ev.charCode)))
@@ -23,7 +25,13 @@ steal.plugins('phui/widget').then(function($){
         },
         select : function(){
             this.element.children('input')[0].focus()
-        }
+        },
+        mouseenter : function(el, ev){
+            el.css('cursor','pointer');
+        },
+        mouseleave : function(el, ev){
+            el.css('cursor','');            
+        }                
     })
     
     
