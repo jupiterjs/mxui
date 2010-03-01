2efc70d4bb8181885401f6032d1c0e2ecb893fae
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-03-01 14:06:31 +0000

    Added label and description to Textbox widget.

--- widget/textbox/textbox.js
+++ widget/textbox/textbox.js
@@ -1,25 +1,30 @@
 steal.plugins('phui/widget').then(function($){
-	
-	
-	Phui.Widget.extend("Phui.Widget.Textbox",
-	{
-		listensTo: ["select"]
-	},
-	{
-		init : function(el, options){
-			this.options = options;
-		},
-		keypress : function(el, ev){
-			if(this.options.allow && ev.charCode && ! this.options.allow.test(String.fromCharCode(ev.charCode)))
-				ev.preventDefault()
-		},
-		value : function(){
-			return this.element.children('input').val();
-		},
-		select : function(){
-			this.element.children('input')[0].focus()
-		}
-	})
-	
-	
+    
+    
+    Phui.Widget.extend("Phui.Widget.Textbox",
+    {
+        defaults : {
+            INPUT_CLASS_NAMES : "",
+            LABEL_CLASS_NAMES : "label",    
+            DESCRIPTION_CLASS_NAMES : "description"
+        },
+        listensTo: ["select"]
+    },
+    {
+        init : function(el, options){
+            this.options = options;
+        },
+        keypress : function(el, ev){
+            if(this.options.allow && ev.charCode && ! this.options.allow.test(String.fromCharCode(ev.charCode)))
+                ev.preventDefault()
+        },
+        value : function(){
+            return this.element.children('input').val();
+        },
+        select : function(){
+            this.element.children('input')[0].focus()
+        }
+    })
+    
+    
 })
