45f9260748298fea989ef5708961ae0812c5f952
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-04-28 21:11:24 +0100

    Progress on grid/tree widget.

--- grid/tree/tree.js
+++ grid/tree/tree.js
@@ -12,7 +12,6 @@ steal.plugins('phui/grid')
     },
     {
         init : function(){
-            this.depth = 0;
             var indentedColumn = this.options.indentedColumn;
             
             if(this.options.render && this.options.render[indentedColumn]){
@@ -22,10 +21,10 @@ steal.plugins('phui/grid')
                     var content = [];
             
                     if (instance.hasChildren()) {
-                        content.push('<span class="toggle ui-icon ui-icon-triangle-1-e" style="float: left; margin-left: ');
+                        content.push('<span class="expand ui-icon ui-icon-triangle-1-e" style="float: left; margin-left: ');
                         content.push(instance.depth * 20);
                         content.push('px; display: block;height:12px;">');
-						content.push('</span>');
+                        content.push('</span>');
                     }
                     content.push(oldRender.apply(this, arguments));
                     return content.join("");
@@ -35,24 +34,78 @@ steal.plugins('phui/grid')
             this._super(this.element[0], this.options)
         },
         
-        ".toggle click" : function(el, ev){
-            var rowEl = el.closest("tr");
-            var instance = rowEl.model();
-            this.options.model.findAll({parentId: instance.id}, this.callback('foundChildren', rowEl, el));
-        },
-        
-        foundChildren : function(rowEl, toggleEl, items){
-			this.depth++;
-			var self = this;
-            $.each(items, function(i, item){
-                if(!item.depth) item.attr("depth", self.depth);
-            })
-            rowEl.after("//phui/grid/tree/views/rows.ejs", {
+        ".expand click" : function(el, ev){
+			var tr = el.closest("tr");
+			var children = tr.data("children"); 
+            if (!children || !children.length) {
+                this.options.model.findAll({
+                    parentId: tr.model().id
+                }, this.callback('foundChildren', el));
+				return;
+            }
+			
+            tr.after("//phui/grid/tree/views/rows.ejs", {
                 options: this.options,
-                items: items
+                children: children,
+				parent: tr.model()
+            });			
+			
+          tr.find(".ui-icon").removeClass("ui-icon-triangle-1-e expand").addClass("ui-icon-triangle-1-s collapse");				
+        },
+		
+		".collapse click" : function(el, ev){
+			var tr = el.closest("tr");
+			this.removeSubTree(tr);
+			
+            tr.find(".ui-icon").removeClass("ui-icon-triangle-1-s collapse").addClass("ui-icon-triangle-1-e expand");			
+		},
+        
+		removeSubTree : function(tr){
+			var self = this;
+			var childrenEls = tr.data ? tr.data("childrenEls") : $(tr).data("childrenEls");
+		    if (childrenEls) {
+				$.each(childrenEls, function(i, el){
+					self.removeSubTree(el);
+				})
+				$(el).remove();
+			}
+		},
+		
+        foundChildren : function(el, children){ 
+            var self = this;
+			
+			var tr = el.closest("tr");
+			var parentEl, parentId, parentMatch = tr.attr("class").match(/parent_([\w\d_]+)/);
+			if (parentMatch && parentMatch[1]) {
+				parentEl = this.find("." + parentMatch[1])[0];
+			}
+			
+		    var depth = parentEl ? $(parentEl).data("depth") + 1 : 1;
+			tr.data("depth", depth);
+			
+			
+			$(children).each(function(i, child){
+				if(!child.depth) child.depth = depth;
+			})
+			 
+            tr.after("//phui/grid/tree/views/rows.ejs", {
+                options: this.options,
+                children: children,
+				parent: tr.model()
             });
 			
-			toggleEl.removeClass("ui-icon-triangle-1-e").addClass("ui-icon-triangle-1-s");
+			var childrenEls = [];
+			$.each(children, function(i, child){
+				var child = self.find("." + child.identity())[0];
+				if(child !== tr[0])
+                    childrenEls.push(child)				
+			});
+			tr.data("childrenEls", childrenEls);
+
+			tr.data("children", children);
+            
+            tr.find(".ui-icon").removeClass("ui-icon-triangle-1-e expand").addClass("ui-icon-triangle-1-s collapse");
+
         }
     })
      
