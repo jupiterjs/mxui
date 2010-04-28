350468366805744386cab8a756eb41735deae63b
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-04-28 14:24:11 +0100

    Use this.options.model.findAll instead of using instance.Class.findAll.

--- grid/tree/tree.js
+++ grid/tree/tree.js
@@ -12,37 +12,39 @@ steal.plugins('phui/grid')
     },
     {
         init : function(){
-			var self = this;
-			var indentedColumn = this.options.indentedColumn;
-			if(this.options.render && this.options.render[indentedColumn]){
-				var oldRender = this.options.render[indentedColumn];
-				this.options.render[indentedColumn] = function(instance){
-					var content = [];
+            var indentedColumn = this.options.indentedColumn;
+            
+            if(this.options.render && this.options.render[indentedColumn]){
+                var oldRender = this.options.render[indentedColumn];
+                
+                this.options.render[indentedColumn] = function(instance){
+                    var content = [];
+            
                     if (instance.hasChildren()) {
                         content.push('<span class="toggle ui-icon ui-icon-folder-collapsed" style="float: left; width: ');
                         content.push(instance.depth * 20);
                         content.push('px; display: block;height:12px;"></span>');
                     }
-					content.push(oldRender.apply(this, arguments));
-					return content.join("");
-				}
-			}
-			
-			new Phui.Grid(this.element[0], this.options)
-		},
-		
-		".toggle click" : function(el, ev){
-			var rowEl = el.closest("tr");
-			var instance = rowEl.model();
-			instance.Class.findAll({parentId: instance.id}, this.callback("childrenLoaded", rowEl));
-		},
-		
-		childrenLoaded : function(rowEl, items){
-			rowEl.after("//phui/grid/tree/views/rows.ejs", {
+                    content.push(oldRender.apply(this, arguments));
+                    return content.join("");
+                }
+            }
+            
+            new Phui.Grid(this.element[0], this.options)
+        },
+        
+        ".toggle click" : function(el, ev){
+            var rowEl = el.closest("tr");
+            var instance = rowEl.model();
+            this.options.model.findAll({parentId: instance.id}, this.callback('foundChildren', rowEl));
+        },
+        
+        foundChildren : function(rowEl, items){
+            rowEl.after("//phui/grid/tree/views/rows.ejs", {
                 options: this.options,
                 items: items
             });
-		}
+        }
     })
      
 })
