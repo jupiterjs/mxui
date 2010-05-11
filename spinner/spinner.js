7fd9519efd1b0a9fa864f5a2c3a8f22296c4db84
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-05-11 23:59:27 +0100

    Doesn't show hover effect on spinner arrows if that spinner is disabled.

--- spinner/spinner.js
+++ spinner/spinner.js
@@ -1,56 +1,55 @@
 steal.plugins('jquery/controller').then(function($){
-	$.Controller.extend("Phui.Spinner",{
-		defaults: {
-			enableUp : true,
-			enableDown : true,
-		}
-	},
-	{
-		init : function(){
-			this.element.html('<div class="spinner-button up "></div><div class="spinner-button down"></div>');
-			this.upEnabled(this.options.enableUp);
-			this.downEnabled(this.options.enableDown)
-		},
-		upEnabled : function(enable){
-			return this.enabled("Up",enable)
-		},
-		downEnabled : function(enable){
-			return this.enabled("Down",enable)
-		},
-		enabled : function(direction, enable){
-			if(enable === undefined){ //getter
-				return this.element.find('.enabled'+direction).length > 0
-			}
-			var el = this.element.find("."+direction.toLowerCase())
-			if(enable){
-				el.addClass("enabled"+direction).attr('tabindex',0)
-			}else{
-				el.removeClass("enabled"+direction).removeAttr('tabindex')
-			}
-		},
-		".spinner-button click" : function(el){
-			if(/enabled/.test(el[0].className))
-				this.element.trigger( el.hasClass("up") ? "increment" : "decrement" );
-		},
-		".spinner-button keypress" : function(el, ev){
-			if(ev.keyCode == 13 && /enabled/.test(el[0].className))
-				this.element.trigger( el.hasClass("up") ? "increment" : "decrement" );
-		},
-				".up mouseenter" : function(el, ev) {
-			el.css("background-image", "up-hover.png");
-		},
-		".up mouseenter" : function(el, ev) {
-            el.addClass("upHover");			
-		},
-		".down mouseenter" : function(el, ev) {
-            el.addClass("downHover");			
-		},
-		".up mouseleave" : function(el, ev) {
-            el.removeClass("upHover");			
-		},
-		".down mouseleave" : function(el, ev) {
-            el.removeClass("downHover");			
-		}						
-
-	})
+    $.Controller.extend("Phui.Spinner", {
+        defaults: {
+            enableUp: true,
+            enableDown: true,
+        }
+    }, {
+        init: function(){
+            this.element.html('<div class="spinner-button up "></div><div class="spinner-button down"></div>');
+            this.upEnabled(this.options.enableUp);
+            this.downEnabled(this.options.enableDown)
+        },
+        upEnabled: function(enable){
+            return this.enabled("Up", enable)
+        },
+        downEnabled: function(enable){
+            return this.enabled("Down", enable)
+        },
+        enabled: function(direction, enable){
+            if (enable === undefined) { //getter
+                return this.element.find('.enabled' + direction).length > 0
+            }
+            var el = this.element.find("." + direction.toLowerCase())
+            if (enable) {
+                el.addClass("enabled" + direction).attr('tabindex', 0)
+            }
+            else {
+                el.removeClass("enabled" + direction).removeAttr('tabindex')
+            }
+        },
+        ".spinner-button click": function(el){
+            if (/enabled/.test(el[0].className)) 
+                this.element.trigger(el.hasClass("up") ? "increment" : "decrement");
+        },
+        ".spinner-button keypress": function(el, ev){
+            if (ev.keyCode == 13 && /enabled/.test(el[0].className)) 
+                this.element.trigger(el.hasClass("up") ? "increment" : "decrement");
+        },
+        ".up mouseenter": function(el, ev){
+            if (/enabled/.test(el[0].className)) 
+                el.addClass("upHover");
+        },
+        ".down mouseenter": function(el, ev){
+            if (/enabled/.test(el[0].className)) 
+                el.addClass("downHover");
+        },
+        ".up mouseleave": function(el, ev){
+            el.removeClass("upHover");
+        },
+        ".down mouseleave": function(el, ev){
+            el.removeClass("downHover");
+        }
+        
+    })
 })
