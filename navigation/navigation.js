66d7f57d258cd4a55b6311b1428a57daa2f517ea
Brian Moschel <brian.moschel@gmail.com>
2010-05-29 18:44:51 -0500

    nav fix

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -113,7 +113,7 @@ steal.plugins('phui/toolbar').then(function(){
 		init : function(){
 			this._super.apply(this, arguments)
 			this.element.mixin(Phui.Shiftable, Phui.Highlight)
-            this.element.children("ul").css("position","relative")
+            this.element.css("position","relative")
 		},
 		"li select:before" : function(el, ev){
 			ev.preventDefault();
