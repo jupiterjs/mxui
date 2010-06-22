ea11998014f5e6759f9fbf104899ffc89612b8c1
Pinhook <contact@pinhooklabs.com>
2010-06-22 01:01:27 -0500

    sortable funcunit tests

--- sortable/test/funcunit/sortable_test.js
+++ sortable/test/funcunit/sortable_test.js
@@ -7,7 +7,20 @@ module("phui/sortable", {
 test("adding an item", function(){
 	S("#drag").dragTo(".sortable:eq(1)").dragTo("#away");
 	
-	$(".sortable:eq(1)").text(function(text){
+	S(".sortable:eq(1)").text(function(text){
 		ok(/SOMETHING ELSE/.test(text), "Something else in page")
+	})
+})
+test("moving items", function(){
+	S(".sortable:eq(0)").dragTo(".sortable:eq(2)");
+	//make sure it's in a new order
+	S(".sortable:eq(0)").text(function(text){
+		ok(/Second/.test(text), "Second is first")
+	})
+	S(".sortable:eq(1)").text(function(text){
+		ok(/First/.test(text), "First is second")
+	})
+	S(".sortable:eq(2)").text(function(text){
+		ok(/Third/.test(text), "Third stays the same")
 	})
 })
