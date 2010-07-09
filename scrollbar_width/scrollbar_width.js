d62ad91fc37cf4b26db50af3980d3a89e938b815
Brian Moschel <brian.moschel@gmail.com>
2010-07-09 15:27:00 -0500

    fixing for IE7

--- scrollbar_width/scrollbar_width.js
+++ scrollbar_width/scrollbar_width.js
@@ -7,14 +7,18 @@ steal.plugins('jquery').then(function($){
 				width: "100px",
 				height: "100px",
 				overflow: "hidden"
-			}).appendTo(document.documentElement),
+			}).appendTo(document.body),
 			inner = $(div[0].childNodes[0]),
 			w1 = inner[0].offsetWidth,
 			w2;
 
 		div.css("overflow","scroll");
-		inner.css("width","100%"); //have to set this here for chrome
+		//inner.css("width","100%"); //have to set this here for chrome
 		var w2 = inner[0].offsetWidth;		
+		if (w2 == w1) {
+			inner.css("width", "100%"); //have to set this here for chrome
+			w2 = inner[0].offsetWidth;
+		}
 		div.remove();
 		(window.Phui || (window.Phui = {}) ).scrollbarWidth = w1 - w2;
 })
