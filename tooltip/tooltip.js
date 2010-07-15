84aab57094d4ca800f2cf3f88ac8b921a9ad5367
Pinhook <contact@pinhooklabs.com>
2010-07-15 10:21:28 -0500

    fixes tooltip

--- tooltip/tooltip.js
+++ tooltip/tooltip.js
@@ -1,14 +1,25 @@
 steal.plugins('jquery/controller',
 			  'jquery/view/ejs',
 			  'phui/positionable')
-     .resources()
-     .models()
-     .controllers()
-     .views()
 	 .then( function($){
 	 	
 		$.Controller.extend("Phui.Tooltip",
 		{
+			init : function(){
+				this._super()
+				//make tooltip element for everyone
+				this.tooltipEl = $("<div class='tooltip'></div>")
+					.hide()
+					.appendTo( $(document.body) )
+					.phui_positionable( {
+						my: 'left top',
+						at: 'left bottom',
+						offset: '10 10',
+						collision: 'none none'
+					});
+				
+			},
+			
 			defaults: {
 				html: "<h1>Hello World</h1>",
 				width: 120,
@@ -20,28 +31,29 @@ steal.plugins('jquery/controller',
 		},
 		{
 			init: function() {
-				this.tooltipEl = $("<div class='tooltip'></div>");
-				this.tooltipEl.appendTo( $(document.body) );
-				this.tooltipEl.css({border: this.options.border,
+				
+				/*this.tooltipEl.css({border: this.options.border,
 									backgroundColor: this.options.backgroundColor,
 									width: this.options.width,
 									height: this.options.height,
 									opacity: this.options.opacity});
 				this.tooltipEl.html(this.options.html);
-				this.tooltipEl.phui_positionable( {
-					my: 'left top',
-					at: 'left bottom',
-					offset: '0 2',
-					collision: 'none none',
-				} );
-				this.tooltipEl.hide();
+				this.tooltipEl );
+				this.tooltipEl.hide();*/
 			},
 			mouseenter: function(el, ev) {
-				this.tooltipEl.trigger("move", this.element);
-				this.tooltipEl.fadeIn("fast");
+				this.Class.tooltipEl
+					.html(this.options.html)
+					.css({border: this.options.border,
+									backgroundColor: this.options.backgroundColor,
+									width: this.options.width,
+									height: this.options.height,
+									opacity: this.options.opacity})
+					.trigger("move", ev)
+					.fadeIn("fast");
 			},
 			mouseleave: function(el, ev) {
-				this.tooltipEl.fadeOut("fast");
+				this.Class.tooltipEl.fadeOut("fast");
 			}
 		});
 		
