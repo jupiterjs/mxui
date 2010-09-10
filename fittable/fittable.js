dae042348c12d1d7e4a6d4e25c286dfc290aed3a
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-09-10 00:17:20 -0500

    Fittable bug fixes.

--- fittable/fittable.js
+++ fittable/fittable.js
@@ -13,40 +13,51 @@ steal.plugins('jquery/dom/dimensions').then(function($){
 		offset: function() { 
 			return { left:0, top:0} 
 		},
-		height: function(){
+		outerHeight: function(){
 			return $(window).height()
 		},
 		scrollTop: function() {
 			return $(window).scrollTop()
-		} 
+		}
 	};
 	
 	var	fit = function( el, of, within ){
-			var op = of.scrollableParent() || fakeWindow,
-				spaceAvailableAbove = Math.abs(op.offset().top - of.offset().top) - op.scrollTop(),
-				spaceAvailableBellow = op.height() - of.offset().top - of.outerHeight(),
-				bellowPosition = { top: of.offset().top + of.outerHeight(), left: of.offset().left };
-			
-			// use it to calculate element's new height
-			var newAboveHeight = spaceAvailableAbove,
-				newBellowHeight = spaceAvailableBellow;	 												
+			var op = of.scrollableParent(),
+				spaceAvailableAbove,
+				spaceAvailableBellow,
+				bellowPosition;
+				
+			if(op) {
+				spaceAvailableAbove = Math.abs(op.offset().top - of.offset().top) 
+						- parseInt( op.curStyles("borderTopWidth").borderTopWidth.replace("px","") )
+						- parseInt( op.curStyles("borderBottomWidth").borderBottomWidth.replace("px","") );
+				spaceAvailableBellow = op.outerHeight() - of.offset().top - of.outerHeight()
+						+ parseInt( op.curStyles("borderTopWidth").borderTopWidth.replace("px","") )
+						+ parseInt( op.curStyles("borderBottomWidth").borderBottomWidth.replace("px","") );
+				bellowPosition = { top: of.offset().top + of.outerHeight(), left: of.offset().left };				
+			} else {
+				op = fakeWindow;
+				spaceAvailableAbove = Math.abs(op.offset().top - of.offset().top) - op.scrollTop();
+				spaceAvailableBellow = op.outerHeight() - of.offset().top - of.outerHeight() + op.scrollTop();
+				bellowPosition = { top: of.offset().top + of.outerHeight(), left: of.offset().left };				
+			}	 												
 				
 			// If the element can be positioned without scrolling below target, draw it
 			if (spaceAvailableBellow >= el.outerHeight()) {
 				el.offset( bellowPosition );
 			} else if( spaceAvailableBellow >= within ) { 
 				// If the element can be positioned with scrolling greater than min height, draw it
-				el.height( newBellowHeight );
+				el.height( spaceAvailableBellow );
 				el.css( "overflow","auto" ); 
 				el.offset( bellowPosition );
 			} else if (spaceAvailableAbove > spaceAvailableBellow) { 
 				// If the space above is greater than the space below, draw it above
-				el.height( newAboveHeight );
+				el.height( spaceAvailableAbove );
 				el.css( "overflow","auto" ); 
 				el.offset( { top: of.offset().top - el.outerHeight(), left: of.offset().left } );
 			} else if (spaceAvailableAbove <= spaceAvailableBellow) { 
 				//  If the space above is less than the space below, draw it to fit in the space remaining
-				el.height( newBellowHeight );
+				el.height( spaceAvailableBellow );
 				el.css( "overflow","auto" ); 
 				el.offset( bellowPosition );					
 			}
