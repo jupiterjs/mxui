3bb9761cd17b25e567a935b7050442365e6357e8
Pinhook <contact@pinhooklabs.com>
2010-12-12 20:58:40 -0600

    nicer formatting

--- slider/slider.js
+++ slider/slider.js
@@ -1,51 +1,50 @@
-steal.plugins('jquery/controller',
-		      'jquery/event/drag/limit',
-			  'jquery/event/drag/step').then(function($){
+steal.plugins('jquery/controller', 
+              'jquery/event/drag/limit', 
+              'jquery/event/drag/step').then(function( $ ) {
 
-	$.Controller("Mxui.Slider",{
-		defaults : {
+	$.Controller("Mxui.Slider", {
+		defaults: {
 			min: 0,
 			max: 10
 		}
-	},{
-		init : function(){
+	}, {
+		init: function() {
 			this.element.css({
 				position: 'relative'
 			})
-			if(this.options.val){
+			if ( this.options.val ) {
 				this.val(this.options.val)
 			}
 		},
-		getDimensions : function(){
+		getDimensions: function() {
 			var spots = this.options.max - this.options.min,
 				parent = this.element.parent();
 			this.widthToMove = parent.width() - this.element.outerWidth();
-			this.widthOfSpot = this.widthToMove  / spots;
-			var styles = parent.curStyles("borderLeftWidth","paddingLeft"),
-				leftSpace = parseInt( styles.borderLeftWidth ) + parseInt( styles.paddingLeft )|| 0
-			this.leftStart = parent.offset().left + leftSpace ;
+			this.widthOfSpot = this.widthToMove / spots;
+			var styles = parent.curStyles("borderLeftWidth", "paddingLeft"),
+				leftSpace = parseInt(styles.borderLeftWidth) + parseInt(styles.paddingLeft) || 0
+				this.leftStart = parent.offset().left + leftSpace;
 		},
-		"draginit" : function(el, ev, drag){
+		"draginit": function( el, ev, drag ) {
 			this.getDimensions();
-			drag.limit(this.element.parent())
-				.step(this.widthOfSpot, this.element.parent());
+			drag.limit(this.element.parent()).step(this.widthOfSpot, this.element.parent());
 		},
-		"dragend" : function(el, ev, drag){
-			var left =  this.element.offset().left - this.leftStart;
-			var spot = Math.round( left / this.widthOfSpot );
-			this.element.trigger("change", spot+this.options.min)
+		"dragend": function( el, ev, drag ) {
+			var left = this.element.offset().left - this.leftStart;
+			var spot = Math.round(left / this.widthOfSpot);
+			this.element.trigger("change", spot + this.options.min)
 		},
-		val : function(value){
+		val: function( value ) {
 			this.getDimensions();
-			if(value){
+			if ( value ) {
 				//move slider into place
 				this.element.offset({
-					left: this.leftStart+Math.round( (value-this.options.min)*this.widthOfSpot )
+					left: this.leftStart + Math.round((value - this.options.min) * this.widthOfSpot)
 				})
 				this.element.trigger("change", value)
-			}else{
-				var left =  this.element.offset().left - this.spaceLeft;
-				return Math.round( this.leftStart / this.widthOfSpot )+this.options.min;
+			} else {
+				var left = this.element.offset().left - this.spaceLeft;
+				return Math.round(this.leftStart / this.widthOfSpot) + this.options.min;
 			}
 		}
 	})
