e93c434017bec6ef3b24a4648e3f77182dfd3f95
Nicolas <nicolas.papon@webflows.fr>
2011-09-19 22:05:19 +0200

    commented model delegation, handle this stuff in application  controllers

--- data/grid/grid.js
+++ data/grid/grid.js
@@ -116,7 +116,7 @@ $.Controller.extend("Mxui.Data.Grid",{
 		if(clear){
 			this.empty();
 		}
-		
+
 		this.append(trs);
 		// update the items
 		this.options.params.attr('count',items.count)
@@ -130,7 +130,7 @@ $.Controller.extend("Mxui.Data.Grid",{
 		}
 	},
 	newRequest : function(attr, val){
-		var clear = true; 
+		var clear = true;
 		if(!this.options.offsetEmpties && attr == "offset"){ // if offset changes and we have offsetEmpties false
 			clear = false;
 		} 
@@ -141,35 +141,35 @@ $.Controller.extend("Mxui.Data.Grid",{
      * @param {Object} called
      * @param {Object} item
      */
-    "{model} updated" : function(model, ev, item){
-        var el = item.elements(this.element).html(this.options.row, item);
-        if(this.options.updated){
-            this.options.updated(this.element, el, item)
-        }
-		this.element.resize()
-    },
-    "{model} created" : function(model, ev, item){
-        var newEl = $($.View("//mxui/data/grid/views/list",{
-            items : [item],
-            row: this.options.row
-        }))
-        if(this.options.append){
-            this.options.append(this.element, newEl, item)
-        }else{
-            this.append(newEl)
-			//newEl.appendTo(this.element).slideDown();
-        }
-    },
-    "{model} destroyed" : function(model, ev, item){
-        var el = item.elements(this.element)
-        if(this.options.remove){
-            this.options.remove(this.element,el, item)
-        }else{
-            el.slideUp( function(){
-                el.remove();
-            })
-        }
-    },
+//    "{model} updated" : function(model, ev, item){
+//        var el = item.elements(this.element).html(this.options.row, item);
+//        if(this.options.updated){
+//            this.options.updated(this.element, el, item)
+//        }
+//		this.element.resize()
+//    },
+//    "{model} created" : function(model, ev, item){
+//        var newEl = $($.View("//mxui/data/grid/views/list",{
+//            items : [item],
+//            row: this.options.row
+//        }))
+//        if(this.options.append){
+//            this.options.append(this.element, newEl, item)
+//        }else{
+//            this.append(newEl)
+//			//newEl.appendTo(this.element).slideDown();
+//        }
+//    },
+//    "{model} destroyed" : function(model, ev, item){
+//        var el = item.elements(this.element)
+//        if(this.options.remove){
+//            this.options.remove(this.element,el, item)
+//        }else{
+//            el.slideUp( function(){
+//                el.remove();
+//            })
+//        }
+//    },
 	/**
 	 * Insert rows into the table
 	 * @param {Object} row insert after this row
