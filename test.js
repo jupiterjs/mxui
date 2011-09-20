e0e317ae00468f312b22ab264b5edcf0d96bff75
Brian Moschel <Brian.Moschel@contractor.cengage.com>
2011-09-20 01:09:44 -0400

    tests working again

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ steal('funcunit')
 	.then('//mxui/layout/fill/fill_test')
 	.then('//mxui/layout/sortable/sortable_test')
 	.then('//mxui/layout/split/split_test')
-	// .then('//mxui/layout/table_scroll/table_scroll_test')
-	// .then('//mxui/layout/wrap/wrap_test')
+	.then('//mxui/layout/table_scroll/table_scroll_test')
+	.then('//mxui/layout/wrap/wrap_test')
 	
 //	'//mxui/layout/block/block_test',
 //	'//mxui/nav/menuable/menuable_test'
