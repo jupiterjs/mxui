2a6dba79adb7ab5b230868120678e9de6e8d2031
Justin Meyer <justinbmeyer@gmail.com>
2011-10-03 00:11:32 -0500

    accordion working with null objects

--- nav/accordion/accordion.js
+++ nav/accordion/accordion.js
@@ -35,13 +35,15 @@ $.Controller("Mxui.Nav.Accordion",{
 			
 		// Select first content element, since children.eq(0) is title, and trigger show and resize height.
 		if(this.options.activateFirstByDefault){
-			this.setCurrent(children.eq(1));
+			this.setActive(children.eq(0));
 		}
 		
 		//- Fixes problems with scrollbars when expanding/collasping elements
 		this.element.css('overflow', 'hidden');
 	},
-	
+	currentContent : function(){
+		return this.element.children(':visible').not(this.options.title)
+	},
 	/**
 	 * Draws the current in the right spot. Triggered initially or when resized.
 	 * @param children
@@ -52,13 +54,14 @@ $.Controller("Mxui.Nav.Accordion",{
 		var titles = (children || this.element.children() ).filter(this.options.title);
 		
 		// Initial proposed height for current content area.
-		var ul_height = 0;
+		var ul_height = 0,
+			content = this.currentContent();
 		
 		// Set current div height to match parent first.
 		//this.element.height($(this.element).parent().height());
 		
 		// Make sure current height is not fixed to prevent double scroll bar.
-		this.current.height('');
+		content.height('');
 		
 		// Calculate the allowed height after subtracting all titles height if not using scrollbar.
 		if(titles && titles.length > 0){
@@ -66,21 +69,29 @@ $.Controller("Mxui.Nav.Accordion",{
 		}
 		// Minimum height for ul area to be 50, if allowed height more than 50, use the allowed height to push remaing titles to the bottom.
 		if(ul_height > 50){
-			this.current.outerHeight(ul_height);
+			content.outerHeight(ul_height);
 		}
 	},
 	
 	/**
-	 * Does the default actions for showing one but not animating.  Used to set default entry.
+	 * Sets
+	 * Does the default actions for showing one 
+	 * but not animating.  Used to set default entry.
+	 * 
 	 * @param {Object} el
 	 * @param {Array} arguments to pass to trigger.
 	 */
-	setCurrent:function(el, args)
+	setActive:function(el, args)
 	{
+		var next = el.next();
+		
 		this.current = el;
-		this.current.triggerHandler("show", args);
-		this.current.show();
-		this.current.prev().addClass(this.options.currentClassName).addClass(this.options.activeClassName);
+		if( !next.is(this.options.title) ) {
+			next.triggerHandler("show", args)
+			next.show();
+		}
+		
+		el.addClass(this.options.currentClassName).addClass(this.options.activeClassName);
 		this.setHeight();
 	},
 	
@@ -110,54 +121,59 @@ $.Controller("Mxui.Nav.Accordion",{
 	 */
 	expand : function(el)
 	{
+		var next = el.next();
 		// If we don't have one selected by default
-		if(!this.current){
-			this.setCurrent(el.next(), arguments);
+		if(!this.current || next.is(this.options.title) ) {
+			this.setActive(el, arguments);
 			return;
 		}
 		
 		// If proposed content for expansion is already expanded, no need to recalculate.
-		if(el.next().get(0) === this.current.get(0)){
-			this.current.find('.activated').removeClass('activated selected');
-			this.current.triggerHandler("show");
+		if( el[0] === this.current[0] ){
+			//this.current.find('.activated').removeClass('activated selected');
+			
+			//this.current.triggerHandler("show");
 			
 			//- active class name could have been removed by a child active item
-			if(!el.hasClass(this.options.activeClassName)){
-				el.addClass(this.options.activeClassName)
-			}
+			//if(!el.hasClass(this.options.activeClassName)){
+			el.addClass(this.options.currentClassName).addClass(this.options.activeClassName);
+			//}
 			
 			return;
 		}
 		
 		//we need to 'knock out' the top border / margin / etc proportinally ...
 		var newHeight = this.calculateRemainder(null, el),
-			newShow = el.next().show().height(0).trigger("show", arguments),
-			oldH3 = this.current.prev();
+			oldContent = this.element.children(':visible').not(this.options.title),
+			newContent = el.next().show().height(0).trigger("show", arguments),
+			oldH3 = this.current;
 			
 		//- toggle the classes
-		newShow.find('.activated').removeClass('activated selected');
+		newContent.find('.activated').removeClass('activated selected');
 		oldH3.removeClass(this.options.currentClassName).removeClass(this.options.activeClassName);
 		el.addClass(this.options.currentClassName).addClass(this.options.activeClassName);
-			
+		
+		console.log(oldContent, newContent, newHeight)	
+		
 		// Animation closing the existing expanded content and removed class for title, then expand the new one.
-		this.current.stop(true, true).animate({outerHeight: "0px"},{
+		oldContent.stop(true, true).animate({outerHeight: "0px"},{
 			complete : function(){
 				$(this).hide();
-				newShow.outerHeight(newHeight);
+				newContent.outerHeight(newHeight);
 			},
 			step : function(val, ani){
 				//- the height will be 0 if there is more accoridans that height available, 
 				//- then we just want to do a auto height.
 				if(newHeight <= 0){
-					newShow.css('height', 'auto')
+					newContent.css('height', 'auto')
 				} else {
-					newShow.outerHeight(newHeight*ani.pos);
+					newContent.outerHeight(newHeight*ani.pos);
 				}
 			},
 			duration : this.options.animationSpeed
 		});
 		
-		this.current = newShow;
+		this.current = el;
 	},
 	
 	/**
@@ -175,8 +191,10 @@ $.Controller("Mxui.Nav.Accordion",{
 		(titles || this.element.children(this.options.title) )
 				.each(function(){
 					total -= $(this).outerHeight(true);
+					
+					console.log($(this).outerHeight(true), this, total)
 				});
-		
+		console.log("a",total)
 		return total;
 	},
 	
