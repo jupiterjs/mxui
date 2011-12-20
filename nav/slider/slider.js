e7518a08f3af72360c9bd531f74f603204bde487
Ralph Holzmann <ralphholzmann@gmail.com>
2011-12-20 12:18:04 -0600

    Updated slider to listen to the resize event and did some other cleanup, as per @justinbmeyer. Fixes the mxui demo resizing.

--- nav/slider/slider.js
+++ nav/slider/slider.js
@@ -12,6 +12,8 @@ steal(
 	 * @description Creates a slider with `min`, `max` and `interval` options.
 	 * Creates a slider with `min`, `max` and `interval` options.
 	 *
+	 * Given the following markup:
+	 *
 	 *		<div class="container">
 	 *			<div id="slider"></div>
 	 *		</div>
@@ -47,6 +49,12 @@ steal(
 	 *
 	 * @param {Object} options - An object literal describing the range,
 	 * interval and starting value of the slider
+	 *
+	 *	- `min` {Number} - The minimum value the slider can go down to.
+	 *	- `max` {Number} - The maximum value the slider can go up to.
+	 *	- `interval` {Number} - The step size that the slider should increment
+	 *	by when being moved.	
+	 *	- `val` {Number} - The initial starting value of the slider.
 	 */
 	$.Controller("Mxui.Nav.Slider", 
 		/**
@@ -62,31 +70,30 @@ steal(
 		}
 	}, 
 	/**
-	 * @hide
 	 * @prototype
 	 */
 	{
 		init: function() {
 			this.element.css("position", 'relative')
 			if ( this.options.val ) {
-				this.val(this.options.val)
+				this.val( this.options.val )
 			}
+		},
+		resize: function() {
+			this.updatePosition();
 		},
 		getDimensions: function() {
 			var spots = this.options.max - this.options.min,
 				parent = this.element.parent(),
-				outerWidth = this.element.outerWidth();
-			this.widthToMove = parent.width();
-			if(this.options.contained){
-				this.widthToMove = this.widthToMove - outerWidth
-			}else{
-				//this.widthToMove = this.widthToMove + outerWidth
-			}
+				outerWidth = this.element.outerWidth(),
+				styles, leftSpace;
+
+			this.widthToMove = parent.width() - outerWidth;
 			this.widthOfSpot = this.widthToMove / spots;
-			var styles = parent.curStyles("borderLeftWidth", "paddingLeft"),
-				leftSpace = parseInt(styles.borderLeftWidth) + parseInt(styles.paddingLeft) || 0
-				this.leftStart = parent.offset().left + leftSpace -
-				(this.options.contained ? 0 : Math.round(outerWidth / 2));
+
+			styles = parent.curStyles("borderLeftWidth", "paddingLeft");
+			leftSpace = parseInt(styles.borderLeftWidth) + parseInt(styles.paddingLeft) || 0;
+			this.leftStart = parent.offset().left + leftSpace - (this.options.contained ? 0 : Math.round(outerWidth / 2));
 		},
 		"draginit": function( el, ev, drag ) {
 			this.getDimensions();
@@ -94,9 +101,19 @@ steal(
 				.step(this.widthOfSpot, this.element.parent());
 		},
 		"dragmove": function( el, ev, drag ) {
+			this.determineValue();
+			this.element.trigger( "change", this.value )
+		},
+		determineValue : function() {
 			var left = this.element.offset().left - this.leftStart;
 			var spot = Math.round(left / this.widthOfSpot);
-			this.element.trigger("change", spot + this.options.min)
+			this.value = spot + this.options.min;
+		},
+		updatePosition: function() {
+			this.getDimensions();
+			this.element.offset({
+				left: this.leftStart + Math.round((this.value - this.options.min) * this.widthOfSpot)
+			})
 		},
 		/**
 		 * @param {Number} value - Optional. The new value for the slider. If
@@ -104,16 +121,14 @@ steal(
 		 * @return {Number}
 		 */
 		val: function( value ) {
-			this.getDimensions();
 			if ( value !== undefined) {
-				//move slider into place
-				this.element.offset({
-					left: this.leftStart + Math.round((value - this.options.min) * this.widthOfSpot)
-				})
-				this.element.trigger("change", value)
+				this.value = value;
+				this.updatePosition();
+				setTimeout(this.proxy(function() {
+					this.element.trigger("change", value)
+				}), 0);
 			} else {
-				var left = this.element.offset().left - this.spaceLeft;
-				return Math.round(this.leftStart / this.widthOfSpot) + this.options.min;
+				return this.value;
 			}
 		}
 	})
